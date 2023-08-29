ff190335adfb39bdc25952351576d373e8e6dabc
Erick <ericksoares316@gmail.com>
2023-08-29 08:30:34 -0300

    Adiciona a função de compra

--- loja/urls.py
+++ loja/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',Index.as_view(),name='index'),
-    
+    path('',IndexView.as_view(),name='index'),
+    path('produto/<int:pk>',ProdutoView.as_view(),name='detalhe'),
 ]
--- loja/views.py
+++ loja/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.views.generic import ListView, DetailView
 from .models import *
 
-class Index (ListView):
+class IndexView (ListView):
    model = Produto
    template_name= "index.html"
    context_object_name = 'produtos'
 
+class ProdutoView(DetailView):
+    model = Produto
+    template_name = "produto.html"
+   
+    def post(self, request, *args, **kwargs):
+        produto = self.get_object()
+        produto.qtd -= 1
+        produto.save()
+        return redirect('index')
